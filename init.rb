bba2800d74ab5d18ab73dc19a8a01f6d9ace92b4
Oleg Kandaurov <kandaurovoleg@gmail.com>
2012-02-17 12:37:34 +0400

    Added sidebar links to home and projects pages

--- init.rb
+++ init.rb
@@ -5,6 +5,6 @@ Redmine::Plugin.register :redmine_sidebar_links do
   name 'Redmine Sidebar Links plugin'
   author 'Oleg Kandaurov'
   description 'Adds a links to project sidebar'
-  version '0.0.1'
+  version '0.0.2'
   author_url 'http://okandaurov.info'
 end
--- lib/sidebar_links/hooks.rb
+++ lib/sidebar_links/hooks.rb
@@ -1,6 +1,5 @@
 module SidebarLinks
   class Hooks < Redmine::Hook::ViewListener
-    render_on :view_projects_show_sidebar_bottom,
-              :partial => 'hooks/sidebar_links/view_projects_show_sidebar_bottom'
+    render_on :view_layouts_base_sidebar, :partial => 'hooks/sidebar_links/show_sidebar'
   end
 end
